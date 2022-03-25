db0be322c823f4d7086de96dfc8e219cd5579772
Evely-27 <evelysantos27@gmail.com>
2022-03-25 10:58:45 -0300

    configuração do typescript+arquivo teste+index html

---
+++ teste.js
@@ -0,0 +1,3 @@
+"use strict";
+var explicacao = "Construção de 3 classes: produto,carro e motorista.";
+console.log(explicacao);
---
+++ teste.ts
@@ -0,0 +1,2 @@
+var explicacao: string = "Construção de 3 classes: produto,carro e motorista."
+console.log(explicacao);
